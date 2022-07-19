bf8ecc1af54983c8845ec218423fda5c0ad3fb06
torsk-technologies <torsktechnologies@gmail.com>
2022-07-19 20:22:54 +0200

    working

--- app/src/main/java/xyz/torsktechnologies/simplegoals/ui/theme/Type.kt
+++ app/src/main/java/xyz/torsktechnologies/simplegoals/ui/theme/Type.kt
@@ -1,6 +1,6 @@
 package xyz.torsktechnologies.simplegoals.ui.theme
 
-import androidx.compose.material3.Typography
+import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -8,13 +8,13 @@ import androidx.compose.ui.unit.sp
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
+//    bodyLarge = TextStyle(
+//        fontFamily = FontFamily.Default,
+//        fontWeight = FontWeight.Normal,
+//        fontSize = 16.sp,
+//        lineHeight = 24.sp,
+//        letterSpacing = 0.5.sp
+//    )
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
